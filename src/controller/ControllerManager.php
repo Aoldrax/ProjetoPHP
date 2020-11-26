cee1aadf286e7398ae1c74884f5d23676cad4e03
Devwarlt <devwarlt@loesoft.org>
2020-11-26 18:47:03 -0200

    Properly error formatting using BS4

--- src/controller/ControllerManager.php
+++ src/controller/ControllerManager.php
@@ -35,6 +35,26 @@ final class ControllerManager
 
     public function handler(string $controller, array $args): void
     {
-        self::$controllers[$controller]->handler($args);
+        try {
+            self::$controllers[$controller]->handler($args);
+        } catch (\Error $err) {
+            echo "
+            <head><link rel='stylesheet' href='../assets/css/bootstrap.css' /></head>
+            <div class='card text-white bg-danger' style='padding: 4px; width: 100%'>
+            <div class='text-warning card-header'><strong>&tridot;&nbsp;&nbsp;<u>Error detected!</u></strong></div>
+            <div class='card-body'>
+                <h5>Error:</h5>&nbsp;
+                <code class='text-warning bg-dark border-warning rounded'>&nbsp;" . $err->getMessage() . "&nbsp;</code>
+            </div>
+            <div class='card-footer'>
+                <h5>Error::errorInfo():</h5>
+                <p><small><strong>&blacktriangleright;&nbsp;Error Code:</strong> " . $err->getCode() . "</small></p>
+                <p><small><strong>&blacktriangleright;&nbsp;File:</strong> " . $err->getFile() . "</small></p>
+                <p><small><strong>&blacktriangleright;&nbsp;Line:</strong> " . $err->getLine() . "</small></p>
+                <p><small><strong>&blacktriangleright;&nbsp;Stack Trace:</strong> " . implode("<br/>&nbsp;&nbsp;&nbsp;&nbsp;&diamond;&nbsp;<strong>Error</strong> #", explode("#", $err->getTraceAsString())) . "</small></p>
+            </div>
+        </div>
+            ";
+        }
     }
 }
