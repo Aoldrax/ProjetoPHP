150cc665b0e958a4835ff9dba86fbbc6989b9ddb
Devwarlt <devwarlt@loesoft.org>
2020-11-29 21:46:26 -0200

    Update listarUsuarios.php

--- src/view/listarUsuarios.php
+++ src/view/listarUsuarios.php
@@ -1,19 +1,60 @@
-<?php include_once("../assets/header.html") ?>
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Listar Usuarios</title>
-</head>
-
-<body>
 <?php
+
 include "../php/PhpUtils.php";
-include "../controller/ControllerManager.php";
 
 use controller\ControllerManager;
+use php\PhpUtils;
+
+session_start();
+
+if (!isset($_SESSION["usuario"])) {
+    PhpUtils::getSingleton()->onRawIndexErr("É necessário realizar login para acessar esta página!", "/view/");
+    return;
+}
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <?php include "../assets/header.html"; ?>
+    <title>Listar Usuários</title>
+</head>
+
+<body style="height: 100%;" class="alert-light">
+<?php
+if (isset($_GET['err']))
+    echo '
+        <div class="alert alert-danger">
+            <p>' . urldecode($_GET['err']) . '</p>
+        </div>
+        <hr/>
+        ';
+if (isset($_GET['success']))
+    echo '
+        <div class="alert alert-success">
+            <p>' . urldecode($_GET['success']) . '</p>
+        </div>
+        <hr/>
+        ';
+?>
+<table class="table" border="0" width="100%">
+    <tr>
+        <td width="75%">
+            <p class="card-header text-light">
+                Seja bem-vindo(a), <strong><?php echo $_SESSION["usuario"]; ?></strong>!
+            </p>
+        </td>
+        <td align="right">
+            <form action="../php/MVCRouter.php" method="post">
+                <input type="hidden" name="controller" value="usuario"/>
+                <input type="hidden" name="action" value="logout"/>
+                <input class="font-weight-bold btn btn-lg btn-outline-danger" type="submit" value="Sair"/>
+            </form>
+        </td>
+    </tr>
+</table>
+<?php
+
+include "../controller/ControllerManager.php";
 
 $args = array(
     "controller" => "usuario",
@@ -26,7 +67,6 @@ if ($manager->controllerExists($controller))
 else
     $utils->onRawIndexErr("Controlador '<strong>" . $controller . "</strong>' indefinido!", $errRef);
 ?>
-<h2><a href="../controller/logout.php">Sair</a></h2>
 </body>
 
 </html>
