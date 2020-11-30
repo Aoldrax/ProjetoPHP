b0b58a4f6bfdc7dd6fce43d6320a134a8850e1d8
Devwarlt <devwarlt@loesoft.org>
2020-11-29 23:42:25 -0200

    Update editarFilmes.php

--- src/view/editarFilmes.php
+++ src/view/editarFilmes.php
@@ -11,32 +11,34 @@
 
 <body>
 
-    <!--jquery-->
-    <center>
-        <h1 aling="center" style="background-color: #e63535;width:380px; color:whitesmoke;" class="rounded-bottom">Editar Filmes</h1>
+<!--jquery-->
+<center>
+    <h1 aling="center" style="background-color: #e63535;width:380px; color:whitesmoke;" class="rounded-bottom">Editar
+        Filmes</h1>
 
-        <form action="../php/MVCRouter.php" method="post" style="background-color: #fff; height: 910px; width:380px;" class="rounded">
-            <div class="form-group">
-                <p>Nome do Filme</p>
-                <input type="text" name="" value="Monstros S.A.">
-            </div>
-            <div class="form-group">
-                <p>Duração</p>
-                <input type="text" name="" value="1h 36m">
-            </div>
-            <div class="form-group">
-                <p>Nome do Diretor</p>
-                <input type="text" name="" value="Pete Docter">
-            </div>
-            <div class="form-group">
-                <p>Data de Lançamento</p>
-                <input type="text" style="width: 88px;" name="" value="14/11/2001">
-            </div>
-            </br>
-            </br><input type="submit" name="" value="Editar" class="btn btn-danger">
-        </form>
-    </center>
-    <h2><a href="../controller/logout.php">Sair</a></h2>
+    <form action="../php/MVCRouter.php" method="post" style="background-color: #fff; height: 910px; width:380px;"
+          class="rounded">
+        <div class="form-group">
+            <p>Nome do Filme</p>
+            <input type="text" name="" value="Monstros S.A.">
+        </div>
+        <div class="form-group">
+            <p>Duração</p>
+            <input type="text" name="" value="1h 36m">
+        </div>
+        <div class="form-group">
+            <p>Nome do Diretor</p>
+            <input type="text" name="" value="Pete Docter">
+        </div>
+        <div class="form-group">
+            <p>Data de Lançamento</p>
+            <input type="text" style="width: 88px;" name="" value="14/11/2001">
+        </div>
+        </br>
+        </br><input type="submit" name="" value="Editar" class="btn btn-danger">
+    </form>
+</center>
+<h2><a href="../controller/logout.php">Sair</a></h2>
 </body>
 
 </html>
