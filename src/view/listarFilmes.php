080003f87fc28bf033c51f7542bf2aafe46bed04
Devwarlt <devwarlt@loesoft.org>
2020-11-29 23:42:05 -0200

    Update listarFilmes.php

--- src/view/listarFilmes.php
+++ src/view/listarFilmes.php
@@ -12,32 +12,32 @@ include_once("../assets/header.html");
 </head>
 
 <body>
-    <table class="table" style="background-color: #fff; width:1920px; margin: 1px;">
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">Código</th>
-            <th scope="col">Nome do Filme</th>
-            <th scope="col">Duração</th>
-            <th scope="col">Nome do Diretor</th>
-            <th scope="col">Data de Lançamento</th>
-            <th></th>
-            <th></th>
-        </tr>
-        <?php ?>
-        <tr>
-            <th scope="row">1</th>
-            <td>01</td>
-            <td>Monstros S.A.</td>
-            <td>1h 36m</td>
-            <td>Pete Docter</td>
-            <td>14/11/2001</td>
-            <td><input class="btn btn-danger" type="button" value="Deletar" ></td>
-            <td><input class="btn btn-warning" type="button" value="Editar"></td>
-        </tr>
-        <?php ?>
-        <table>
-      
-<h2><a href="../controller/logout.php">Sair</a></h2>
+<table class="table" style="background-color: #fff; width:1920px; margin: 1px;">
+    <tr>
+        <th scope="col">#</th>
+        <th scope="col">Código</th>
+        <th scope="col">Nome do Filme</th>
+        <th scope="col">Duração</th>
+        <th scope="col">Nome do Diretor</th>
+        <th scope="col">Data de Lançamento</th>
+        <th></th>
+        <th></th>
+    </tr>
+    <?php ?>
+    <tr>
+        <th scope="row">1</th>
+        <td>01</td>
+        <td>Monstros S.A.</td>
+        <td>1h 36m</td>
+        <td>Pete Docter</td>
+        <td>14/11/2001</td>
+        <td><input class="btn btn-danger" type="button" value="Deletar"></td>
+        <td><input class="btn btn-warning" type="button" value="Editar"></td>
+    </tr>
+    <?php ?>
+    <table>
+
+        <h2><a href="../controller/logout.php">Sair</a></h2>
 </body>
 
 </html>
