1f621f524899c23a2db62d4ba45cca3cde7375db
Devwarlt <devwarlt@loesoft.org>
2020-11-29 23:42:21 -0200

    Update cadastroFilmes.php

--- src/view/cadastroFilmes.php
+++ src/view/cadastroFilmes.php
@@ -11,31 +11,33 @@
 
 <body>
 
-    <!--jquery-->
-    <center>
-        <h1 aling="center" style="background-color: #e63535;width:380px; color:whitesmoke;" class="rounded-bottom">Cadastrar Filmes</h1>
+<!--jquery-->
+<center>
+    <h1 aling="center" style="background-color: #e63535;width:380px; color:whitesmoke;" class="rounded-bottom">Cadastrar
+        Filmes</h1>
 
-        <form action="../php/MVCRouter.php" method="post" style="background-color: #fff; height: 910px; width:380px;" class="rounded">
-            <div class="form-group">
-                <p>Nome do Filme</p>
-                <input type="text" name="" placeholder="Insira o nome do filmes">
-            </div>
-            <div class="form-group">
-                <p>Duração</p>
-                <input type="text" name="" placeholder="Insira a duração do filme">
-            </div>
-            <div class="form-group">
-                <p>Nome do Diretor</p>
-                <input type="text" name="" placeholder="Insira o nome do diretor">
-            </div>
-            <div class="form-group">
-                <p>Data de Lançamento</p>
-                <input type="date" name="" placeholder="Insira o data de lançamento">
-            </div>
-            </br>
-            </br><input type="submit" name="" value="cadastro" class="btn btn-danger">
-        </form>
-    </center>
+    <form action="../php/MVCRouter.php" method="post" style="background-color: #fff; height: 910px; width:380px;"
+          class="rounded">
+        <div class="form-group">
+            <p>Nome do Filme</p>
+            <input type="text" name="" placeholder="Insira o nome do filmes">
+        </div>
+        <div class="form-group">
+            <p>Duração</p>
+            <input type="text" name="" placeholder="Insira a duração do filme">
+        </div>
+        <div class="form-group">
+            <p>Nome do Diretor</p>
+            <input type="text" name="" placeholder="Insira o nome do diretor">
+        </div>
+        <div class="form-group">
+            <p>Data de Lançamento</p>
+            <input type="date" name="" placeholder="Insira o data de lançamento">
+        </div>
+        </br>
+        </br><input type="submit" name="" value="cadastro" class="btn btn-danger">
+    </form>
+</center>
 </body>
 
 </html>
